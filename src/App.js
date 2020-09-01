17b04778ed79d58a42da749b48d4b3eb7378255c
Kamil Szewczyk <kamilszewczyk0@gmail.com>
2020-09-01 16:46:33 +0200

    added html  template and transformed to JSX

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
 import React from "react";
 
 function App() {
-  return <div>Dzień Dobry!</div>;
+  return (
+    <div>
+      <main className="container">
+        <section className="section">
+          <h1 className="section__header--main">ToDo List</h1>
+          <label className="section__label">Add new task</label>
+          <form className="section__form js-form">
+            <input
+              className="section__input js-newTask"
+              placeholder="What needs to be done?"
+            />
+            <button className="section__btn js-addNewTaskBtn">Add task</button>
+          </form>
+        </section>
+        <section className="section">
+          <section className="section__header">
+            <h2 className="section__title">Task List</h2>
+            <div className="buttons js-buttons"></div>
+          </section>
+          <ul className="section__list js-tasks"></ul>
+        </section>
+      </main>
+    </div>
+  );
 }
 
 export default App;
